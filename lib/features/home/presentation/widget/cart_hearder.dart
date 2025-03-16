25859c0f81e164f61ed6b7a4c3b3f66f6ea249ef
ibtissam <ibtissamwannas21@gmail.com>
2025-03-16 11:29:02 +0200

    Add cart item management: implement CartItemCubit and CartItemEntity; create CartItemActionButtons for quantity adjustments; update CartHeader and CartViewBody to reflect cart state; add CarItemsList for displaying cart items

--- lib/features/home/presentation/widget/cart_hearder.dart
+++ lib/features/home/presentation/widget/cart_hearder.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fresh_cart/features/home/presentation/cubit/cart_cubit/cart_cubit.dart';
 
 class CartHeader extends StatelessWidget {
   const CartHeader({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // ${context.watch<CartCubit>().cartEntity.cartItems.length}
     return Container(
       width: double.infinity,
       padding: const EdgeInsets.symmetric(vertical: 20),
       decoration: const BoxDecoration(color: Color(0xFFEBF9F1)),
-      child: const Center(
+      child: Center(
         child: Text(
-          'لديك منتجات في سله التسوق',
-          style: TextStyle(
+          'لديك ${context.watch<CartCubit>().cartEntity.cartItems.length} منتجات في سله التسوق',
+          style: const TextStyle(
             color: Color(0xFF1B5E37),
             fontSize: 13,
             fontFamily: 'Cairo',
--- lib/features/home/presentation/widget/cart_view_body.dart
+++ lib/features/home/presentation/widget/cart_view_body.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fresh_cart/constants.dart';
 import 'package:fresh_cart/core/widgets/custom_app_bar.dart';
+import 'package:fresh_cart/features/home/presentation/cubit/cart_cubit/cart_cubit.dart';
 import 'package:fresh_cart/features/home/presentation/widget/cart_hearder.dart';
+import 'package:fresh_cart/features/home/presentation/widget/cart_item_list.dart';
+import 'package:fresh_cart/features/home/presentation/widget/custom_cart_button.dart';
 
 class CartViewBody extends StatelessWidget {
   const CartViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Stack(
+    return Stack(
       children: [
         CustomScrollView(
           slivers: [
-            SliverToBoxAdapter(
+            const SliverToBoxAdapter(
               child: Column(
                 children: [
                   SizedBox(
@@ -31,27 +35,27 @@ class CartViewBody extends StatelessWidget {
                 ],
               ),
             ),
-            // SliverToBoxAdapter(
-            //   child: context.read<CartCubit>().cartEntity.cartItems.isEmpty
-            //       ? SizedBox()
-            //       : CustomDivider(),
-            // ),
-            // CarItemsList(
-            //   carItems: context.watch<CartCubit>().cartEntity.cartItems,
-            // ),
-            // SliverToBoxAdapter(
-            //   child: context.read<CartCubit>().cartEntity.cartItems.isEmpty
-            //       ? SizedBox()
-            //       : CustomDivider(),
-            // ),
+            SliverToBoxAdapter(
+              child: context.read<CartCubit>().cartEntity.cartItems.isEmpty
+                  ? const SizedBox()
+                  : const CustomDivider(),
+            ),
+            CarItemsList(
+              carItems: context.watch<CartCubit>().cartEntity.cartItems,
+            ),
+            SliverToBoxAdapter(
+              child: context.read<CartCubit>().cartEntity.cartItems.isEmpty
+                  ? const SizedBox()
+                  : const CustomDivider(),
+            ),
           ],
         ),
-        // Positioned(
-        //   left: 16,
-        //   right: 16,
-        //   bottom: MediaQuery.sizeOf(context).height * .07,
-        //   child: CustomCartButton(),
-        // )
+        Positioned(
+          left: 16,
+          right: 16,
+          bottom: MediaQuery.sizeOf(context).height * .07,
+          child: const CustomCartButton(),
+        )
       ],
     );
   }
